c36e596cdd16a08f233a41687347d7c2c2a61460
Felipe Godoi <feeh.08.2001@gmail.com>
2023-04-28 08:30:23 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+"# CNN-Cars" 
+"# CNN-Cars" 
---
+++ main.py
@@ -0,0 +1,42 @@
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
+from keras.layers import BatchNormalization
+from keras.preprocessing.image import ImageDataGenerator
+
+classificador = Sequential()
+classificador.add(
+    Conv2D(32, (3, 3), input_shape=(64, 64, 3), activation='relu'))
+classificador.add(BatchNormalization())
+classificador.add(MaxPooling2D(pool_size=(2, 2)))
+
+classificador.add(
+    Conv2D(32, (3, 3), input_shape=(64, 64, 3), activation='relu'))
+classificador.add(BatchNormalization())
+classificador.add(MaxPooling2D(pool_size=(2, 2)))
+
+classificador.add(Flatten())
+
+classificador.add(Dense(units=747, activation='relu'))
+classificador.add(Dense(units=747, activation='relu'))
+classificador.add(Dense(units=9, activation='softmax'))
+
+classificador.compile(
+    optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+dados_treinamento = ImageDataGenerator(rescale=1./255)
+dados_teste = ImageDataGenerator(rescale=1./255)
+
+amostras_treinamento = dados_treinamento.flow_from_directory(
+    './training', target_size=(64, 64), batch_size=747, class_mode='categorical')
+
+amostras_teste = dados_teste.flow_from_directory(
+    './test', target_size=(64, 64), batch_size=83, class_mode='categorical')
+
+classificador.fit(amostras_treinamento, steps_per_epoch=16,
+                            epochs=5, validation_data=amostras_teste, validation_steps=80)
+
+# dados_previsao = dados_treinamento.flow_from_directory(
+#     './previsao', target_size=(64, 64), batch_size=1, class_mode='categorical')
+
+# previsao = classificador.predict(dados_previsao)
+# print(previsao)
